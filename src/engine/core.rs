2098905a237847987ef958b9e036cc64838684f7
Will Farris <9190155+WillFarris@users.noreply.github.com>
2021-10-27 17:33:30 -0700

    engine

--- src/engine/core.rs
+++ src/engine/core.rs
@@ -1,12 +1,58 @@
 use super::shader;
 
-pub fn init_engine() {
-    let block_shader = shader::Shader::new(include_str!("../../shaders/block_vertex.glsl"), include_str!("../../shaders/block_fragment.glsl"));
-    let grass_shader = shader::Shader::new(include_str!("../../shaders/grass_vertex.glsl"), include_str!("../../shaders/block_fragment.glsl"));
-    let leaves_shader = shader::Shader::new(include_str!("../../shaders/leaves_vertex.glsl"), include_str!("../../shaders/block_fragment.glsl"));
+enum EngineState {
+    Running,
+    Paused,
+}
 
-    let gui_shader = shader::Shader::new(include_str!("../../shaders/gui_vertex.glsl"), include_str!("../../shaders/gui_fragment.glsl"));
-    //let block_icon_shader = shader::Shader::new(include_str!("../../shaders/gui_vertex.glsl"), include_str!("../../shaders/gui_fragment.glsl"));
+pub(crate) struct Engine {
+    state: EngineState,
+    world: Option<u32>,
+    player: Option<u32>,
 
+    pub(crate) x: f32,
+    pub(crate) y: f32,
+}
+
+pub(crate) static mut ENGINE: Engine = Engine {
+    state: EngineState::Paused,
+    world: None,
+    player: None,
+    x: 1.0,
+    y: 0.0,
+};
+
+impl Engine {
+
+    pub fn start_engine(&mut self) {
+        gl::load_with(|s| unsafe { std::mem::transmute(egli::egl::get_proc_address(s)) });
+        
+        let block_shader = shader::Shader::new(include_str!("../../shaders/block_vertex.glsl"), include_str!("../../shaders/block_fragment.glsl"));
+        let grass_shader = shader::Shader::new(include_str!("../../shaders/grass_vertex.glsl"), include_str!("../../shaders/block_fragment.glsl"));
+        let leaves_shader = shader::Shader::new(include_str!("../../shaders/leaves_vertex.glsl"), include_str!("../../shaders/block_fragment.glsl"));
+    
+        let gui_shader = shader::Shader::new(include_str!("../../shaders/gui_vertex.glsl"), include_str!("../../shaders/gui_fragment.glsl"));
+
+        self.state = EngineState::Running;
+    }
+
+    pub fn tick(&mut self) {
+        
+
+    }
+
+    pub fn render(&mut self) {
+        unsafe {
+            gl::ClearColor(self.x, self.y, 0.0, 0.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+    }
+
+    pub fn set_xy(&mut self, nx: f32, ny: f32) {
+        self.x = nx;
+        self.y = ny;
+    }
 
 }
+
+
--- src/java_interface.rs
+++ src/java_interface.rs
@@ -7,8 +7,7 @@ use self::jni::sys::jstring;
 use jni::sys::jfloat;
 use std::ffi::CString;
 
-static mut x: f32 = 0.0;
-static mut y: f32 = 0.0;
+use crate::engine::core::ENGINE;
 
 #[no_mangle]
 pub unsafe extern fn Java_org_farriswheel_voxelgame_RustInterface_test(env: JNIEnv, _: JClass) -> jstring {
@@ -18,19 +17,13 @@ pub unsafe extern fn Java_org_farriswheel_voxelgame_RustInterface_test(env: JNIE
 }
 
 #[no_mangle]
-pub unsafe extern fn Java_org_farriswheel_voxelgame_RustInterface_setXY(_: JNIEnv, nxj: jfloat, nyj: jfloat) {
-    let nx: f32 = f32::from(nxj);
-    x = nx / 1400.0;
-
-    let ny: f32 = f32::from(nyj);
-    y = ny / 2560.0;
+pub unsafe extern fn Java_org_farriswheel_voxelgame_RustInterface_setXY(_env: JNIEnv, nxj: jfloat, nyj: jfloat) {
+    ENGINE.set_xy(f32::from(nxj), f32::from(nyj));
 }
 
 #[no_mangle]
 pub unsafe extern fn Java_org_farriswheel_voxelgame_RustInterface_onSurfaceCreated() {
-    gl::load_with(|s| unsafe { std::mem::transmute(egli::egl::get_proc_address(s)) });
-
-    crate::engine::core::init_engine();
+    ENGINE.start_engine();
 }
 
 #[no_mangle]
@@ -40,6 +33,5 @@ pub unsafe extern fn Java_org_farriswheel_voxelgame_RustInterface_onSurfaceChang
 
 #[no_mangle]
 pub unsafe extern fn Java_org_farriswheel_voxelgame_RustInterface_onDrawFrame() {
-    gl::ClearColor(x, y, 0.0, 0.0);
-    gl::Clear(gl::COLOR_BUFFER_BIT);
+    ENGINE.render();
 }
