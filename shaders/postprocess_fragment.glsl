133725d6dbfd204c2cf3cd02de51202478faf39d
Will Farris <9190155+WillFarris@users.noreply.github.com>
2022-09-01 14:33:27 -0700

    clean shader code

--- shaders/postprocess_fragment.glsl
+++ shaders/postprocess_fragment.glsl
@@ -28,51 +28,13 @@ void main(){
     vec2 c = uv-0.5;
 
     float depth = linearize_depth(texture(depthTexture, uv).r,zn,zf) / zf;
-
     float center_depth = linearize_depth(texture(depthTexture, vec2(0.5, 0.5)).r,zn,zf);
-    
-    /*for(int y=-1;y<=1;++y) {
-        for(int x=-1;x<=1;++x) {
-            vec2 offset = vec2(float(x), float(y));
-            center_depth += linearize_depth(
-                texture(
-                    depthTexture,
-                    vec2(0.5, 0.5) + offset
-                ).r,
-                zn,zf
-            );
-        }
-    }
-    center_depth /= 9.0;*/
     
     // Bright in center, dark in corners
     float vignette = c.x * c.x + c.y * c.y;
 
-    // Blur more at the edges of the screen
-    //int radius = abs(int(5.0 * abs(depth - center_depth)));
-
-    vec4 blurred_color = texture( renderedTexture, gl_FragCoord.xy/resolution.xy);
-    float count = 0.01;
-    /*for(int y=-radius;y<=radius;++y) {
-        for(int x=-radius;x<=radius;++x) {
-            vec2 cur_coord = gl_FragCoord.xy - vec2(float(x), float(y));
-            cur_coord /= resolution.xy;
-            float cur_depth = linearize_depth(texture(depthTexture, cur_coord).r,zn,zf);
-            if(cur_depth > depth || cur_depth <= 1.0) {
-                blurred_color += texture( renderedTexture, cur_coord);
-                count += 1.0;
-            }
-        }
-    }
-    blurred_color /= count;*/
-
-    vec4 out_color = blurred_color;
+    vec4 out_color = (1.0 - depth) * texture( renderedTexture, gl_FragCoord.xy/resolution.xy);
     out_color *= (1.0 - 0.25 * vignette);
 
-    vec3 direction = vec3(c, 1.0) + camera_forward;
-    
-    //out_color.a *= (1.0 - depth);
-
     color = out_color;
-    //color = vec4(1.0);
 }
--- src/world/block.rs
+++ src/world/block.rs
@@ -1,21 +1,21 @@
-pub const BLOCKS: [Block; 10] = [
+pub const BLOCKS: [Block; 13] = [
     Block {id: 0, name: "Air", solid: false, transparent: true, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: None },
     Block {id: 1, name: "Stone", solid: true, transparent: false, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::Single(1.0, 15.0)) },
     Block {id: 2, name: "Grass", solid: true, transparent: false, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::TopSideBottom((0.0, 15.0), (3.0, 15.0), (2.0, 15.0))) },
     Block {id: 3, name: "Dirt", solid: true, transparent: false, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::Single(2.0, 15.0)) },
-    //Block {id: 4, name: "Cobblestone", solid: true, transparent: false, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::Single(0.0, 14.0)) },
-    //Block {id: 5, name: "Oak Plank", solid: true, transparent: false, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::Single(4.0, 15.0)) },
-    //Block {id: 6, name: "Sand", solid: true, transparent: false, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::Single(2.0, 14.0)) },
     Block {id: 4, name: "Rose", solid: false, transparent: true, block_type: BlockType::Grass, mesh_type: MeshType::CrossedPlanes, texture_map: Some(TextureType::Single(12.0, 15.0))},
-    //Block {id: 8, name: "Glass", solid: true, transparent: true, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::Single(1.0, 12.0))},
     Block {id: 5, name: "Oak Log", solid: true, transparent: false, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::TopSideBottom((5.0, 14.0), (4.0, 14.0), (5.0, 14.0)))},
     Block {id: 6, name: "Dandelion", solid: false, transparent: true, block_type: BlockType::Grass, mesh_type: MeshType::CrossedPlanes, texture_map: Some(TextureType::Single(13.0, 15.0))},
     Block {id: 7, name: "Oak Leaves", solid: true, transparent: true, block_type: BlockType::Leaves, mesh_type: MeshType::Block, texture_map: Some(TextureType::Single(4.0, 12.0))},
     Block {id: 8, name: "Short Grass", solid: false, transparent: true, block_type: BlockType::Grass, mesh_type: MeshType::CrossedPlanes, texture_map: Some(TextureType::Single(7.0, 13.0))},
     Block {id: 9, name: "Fern", solid: false, transparent: true, block_type: BlockType::Grass, mesh_type: MeshType::CrossedPlanes, texture_map: Some(TextureType::Single(8.0, 12.0))},
+    Block {id: 10, name: "Iron Ore", solid: true, transparent: false, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::Single(1.0, 13.0))},
+    Block {id: 11, name: "Coal", solid: true, transparent: false, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::Single(2.0, 13.0))},
+    Block {id: 12, name: "Glass", solid: true, transparent: true, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::Single(1.0, 12.0))},
     //Block {id: 14, name: "Diamond Ore", solid: true, transparent: false, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::Single(2.0, 12.0))},
-    //Block {id: 15, name: "Iron Ore", solid: true, transparent: false, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::Single(1.0, 13.0))},
-    //Block {id: 16, name: "Coal", solid: true, transparent: false, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::Single(2.0, 13.0))},
+    //Block {id: 4, name: "Cobblestone", solid: true, transparent: false, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::Single(0.0, 14.0)) },
+    //Block {id: 5, name: "Oak Plank", solid: true, transparent: false, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::Single(4.0, 15.0)) },
+    //Block {id: 6, name: "Sand", solid: true, transparent: false, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::Single(2.0, 14.0)) },
     //Block {id: 17, name: "Furnace", solid: true, transparent: false, block_type: BlockType::Block, mesh_type: MeshType::Block, texture_map: Some(TextureType::TopSideFrontActivatable((12.0, 13.0),(13.0, 12.0), (13.0, 13.0),(14.0, 12.0)))},
 ];
 
